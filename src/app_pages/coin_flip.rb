e6cf8adc16f50f3449ff25b759adf6343b5e1dd8
Kyle Corlett <corlettkyle5@hotmail.com>
2021-12-14 18:34:41 +1000

    Main part of coin flip code

---
+++ src/app_pages/coin_flip.rb
@@ -0,0 +1,18 @@
+results = ["Heads", "Tails"]
+
+begin
+print "To flip the coin hit the 'Enter' key"
+
+enter = gets.chomp
+if enter == ""
+    puts results.sample
+else
+    raise StandardError
+end
+
+rescue StandardError
+    system "clear"
+    print "Please retry: "
+    retry
+end
+
