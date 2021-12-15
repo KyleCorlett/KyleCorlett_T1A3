8613b2db3ace124d2d5a5d8875e30040bf2219bb
Kyle Corlett <corlettkyle5@hotmail.com>
2021-12-16 08:49:21 +1000

    Added exit

--- src/main.rb
+++ src/main.rb
@@ -16,32 +16,35 @@ while true
     puts pastel.magenta(font.write("CHANCE!"))
     puts "Welcome to the Chance Game!!"
 
-    choice = prompt.select("Select what game you would like to play:", ["Magic 8 Ball", "Coin Flip", "Roll The Dice"])
+    choice = prompt.select("Select what game you would like to play:", ["Magic 8 Ball", "Coin Flip", "Roll The Dice", "Exit"])
 
     case
 
     when choice == "Magic 8 Ball"
 
-        puts pastel.magenta(font.write("Magic 8 Ball"))
         puts "Welcome to the Magic 8 Ball Game"
         puts ""
         enter = prompt.select("Would you like to ask a qustion? ", ["Yes", "No"])
 
+
         if enter == "Yes"
             system "clear"
+            puts pastel.magenta(font.write("Magic 8 Ball"))
             magic_8_ball
         elsif enter == "No"
-
         end
             
     when choice == "Coin Flip"
         system "clear"
+        puts pastel.magenta(font.write("Coin Flip"))
         coin_flip
         return
 
     when choice == "Roll The Dice"
         system "clear"
+        puts pastel.magenta(font.write("Roll The Dice"))
         dice_roll
+    when choice == "Exit"
         return
 end
 end
